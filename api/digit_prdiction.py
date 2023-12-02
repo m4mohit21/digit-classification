500b2a6cf5e7673f996b4345a55204647defe59f
m4mohit21 <m23csa015@iitj.ac.in>
2023-12-02 19:44:20 +0530

    update

--- api/digit_prdiction.py
+++ api/digit_prdiction.py
@@ -7,16 +7,16 @@ app = Flask(__name__)
 @app.route("/compare",methods=["POST"])
 def compare():
     json = request.get_json()
-    image = json["input1"]
-    image = [float(i) for i in image]
+    image1 = json["input1"]
+    image1 = [float(i) for i in image1]
 
     image2 = json["input2"]
     image2 = [float(i) for i in image2]
     model = load("models/svm_gamma:0.0001_C:10.joblib")
     
-    image, image2 = np.array(image).reshape(-1,64), np.array(image2).reshape(-1,64)
+    image1, image2 = np.array(image1).reshape(-1,64), np.array(image2).reshape(-1,64)
     
-    prediction_1, prediction_2 =  model.predict(image), model.predict(image2)
+    prediction_1, prediction_2 =  model.predict(image1), model.predict(image2)
 
     return str(prediction_1 == prediction_2)
 
@@ -24,11 +24,11 @@ def compare():
 @app.route("/predict/<models>",methods=["POST"])
 def prediction(models):
     json = request.get_json()
-    image = json["input"]
-    image = [float(i) for i in image]
+    image1 = json["input"]
+    image1 = [float(i) for i in image1]
     trans = load('./models/transforms.joblib')
-    image = np.array(image).reshape(-1,64)
-    image = trans.transform(image)
+    image1 = np.array(image1).reshape(-1,64)
+    image1 = trans.transform(image1)
     
     
     if models == "svm":
@@ -38,4 +38,4 @@ def prediction(models):
     if models == 'lr':
         model = load(f'./models/M23CSA015_lr_solver:newton-cholesky.joblib')
     
-    return str(model.predict(image)[0])
+    return str(model.predict(image1)[0])
