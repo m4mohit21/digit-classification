28d0e71de7da3a48946cc8aaa291e0f92a595914
m4mohit21 <m23csa015@iitj.ac.in>
2023-12-02 19:36:46 +0530

    update

--- test_utils.py
+++ test_utils.py
@@ -1,4 +1,6 @@
 from utils import get_hyperparameter_combinations
+from sklearn.linear_model import LogisticRegression
+from joblib import dump, load
 
 def test_for_hparam_cominations_count():
     # a test case to check that all possible combinations of paramers are indeed generated
@@ -25,3 +27,14 @@ def test_for_hparam_cominations_values():
     expected_param_combo_2 = {'gamma': 0.01, 'C': 1}
 
     assert (expected_param_combo_1 in params_combinations) and (expected_param_combo_2 in params_combinations)
+
+def test_load_LR():
+    model_fxn = 'lr'
+    params = ["lbfgs", "liblinear", "newton-cg", "newton-cholesky", "sag", "saga"]
+    for params_name in params:
+        model  = load(f'./models/M23CSA015_{model_fxn}_solver:{params_name}.joblib')
+        model_params = model.get_params(deep=True)
+        print(model_params)
+        assert isinstance(model,LogisticRegression)
+        
+        assert model_params['solver']==params_name
